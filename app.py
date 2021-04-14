2a33318535b6646858d0acde0ccc4b01a5f1cd07
Shubham arun gujar <42720425+Gujar-Shubham@users.noreply.github.com>
2021-04-14 13:32:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,145 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from pickle import load
+from numpy import argmax
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+from keras.models import load_model
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'D:/Image Caption Web App/models/model_27.h5'
+Tokenizer = 'D:/Image Caption Web App/models/tokenizer.pkl'
+# Load your trained model
+model = load_model(MODEL_PATH)
+# model._make_predict_function()          # Necessary
+print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+# model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(filename, model):
+    # load the tokenizer
+    tokenizer = load(
+        open('D:/Image Caption Web App/models/tokenizer.pkl', 'rb'))
+    # pre-define the max sequence length (from training)
+    max_length = 34
+    # load the model
+    model = load_model('D:/Image Caption Web App/models/model_27.h5')
+    # load and prepare the photograph
+    photo = extract_features(filename)
+    description = generate_desc(model, tokenizer, photo, max_length)
+    # print(description)
+    return description
+
+# extract features from each photo in the directory
+
+
+def extract_features(filename):
+    # load the model
+    model = VGG16()
+    # re-structure the model
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    # load the photo
+    image = load_img(filename, target_size=(224, 224))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # get features
+    feature = model.predict(image, verbose=0)
+    return feature
+
+# map an integer to a word
+
+
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+# generate a description for an image
+
+
+def generate_desc(model, tokenizer, photo, max_length):
+    # seed the generation process
+    in_text = 'Caption: '
+    # iterate over the whole length of the sequence
+    for i in range(max_length):
+        # integer encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad input
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        # predict next word
+        yhat = model.predict([photo, sequence], verbose=0)
+        # convert probability to integer
+        yhat = argmax(yhat)
+        # map integer to word
+        word = word_for_id(yhat, tokenizer)
+        # stop if we cannot map the word
+        if word is None:
+            break
+        # append as input for generating the next word
+        in_text += ' ' + word
+        # stop if we predict the end of the sequence
+        if word == 'endseq':
+            break
+    final = in_text.split()
+    final = final[0:-1]
+    final = ' '.join(final)
+    return final
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        return preds
+    return render_template('index.html', prediction_text="Caption: {}".format(preds))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
